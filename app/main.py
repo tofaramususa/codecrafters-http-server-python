9be9564b0ed0d58bd1be7afcc97f7cfbef9b2543
tofaramususa <jtofaramususa@gmail.com>
2024-07-25 16:22:37 +0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 # Uncomment this to pass the first stage
 import socket
 
+def handle_request(request):
+
+	requestArray = request.split("\r\n")
+	requestArray = requestArray[0].split(" ")
+	if(requestArray[1] == "/abcdefg"):
+		return 404
+	elif(requestArray[1] == "/"):
+		return 200
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,10 +24,14 @@ def main():
 		print("Server started on localhost 4421")
 		while True:
 			connection, address = server_socket.accept()
-			request = connection.recv(1024)
-			print(request)
+			request = connection.recv(1024).decode()
+			status = handle_request(request)
+
 			try:
-				connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+				if status == 200:
+					connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+				elif status == 404:
+					connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 			except Exception as e:
 				print(f"Error: {e}")
 			finally:
