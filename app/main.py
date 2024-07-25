61161d0e8ad601476556dbd7ec312fa8aa9c33e9
tofaramususa <jtofaramususa@gmail.com>
2024-07-25 15:50:15 +0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,14 +4,27 @@ import socket
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+	print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    server_socket.sendall("HTTP/1.1 200 OK\r\n\r\n")
+	# server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # # server_socket.accept() # wait for client
+	# connection, address = server_socket.accept()
 
+	with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+		print("Server started on localhost 4421")
+		while True:
+			connection, address = server_socket.accept()
+			request = connection.recv(1024)
+			print(request)
+			try:
+				connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+			except Exception as e:
+				print(f"Error: {e}")
+			finally:
+				connection.close()
+			
 
 if __name__ == "__main__":
     main()
