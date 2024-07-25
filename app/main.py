ae948c023d3490c86d33b721142ba3cb2a95699f
tofaramususa <jtofaramususa@gmail.com>
2024-07-26 00:00:15 +0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -36,8 +36,8 @@ def createResponse(content="", content_type="text/plain", status=200, content_en
 	response += f"Content-Type: {content_type}\r\n"
 	response += f"Content-Length: {len(content)}\r\n"
 	response += "\r\n"
-	response += content
-	return response.encode()
+	response = response.encode() + content
+	return response
 
 def handleRequest(request):
 	requestArray = request.split("\r\n") # split request into array
